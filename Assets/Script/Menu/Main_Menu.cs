d2a964019c203ecebd500876919118fa57d685d5
Emiyo-W <68138542+Emiyo-W@users.noreply.github.com>
2020-11-22 16:08:50 +0900

    Menuスクリプト
    ゲームシーンのと各メニュー項目の遷移とゲームの終了機能を追加
    （SettingとDescriptionの機能は今後補足）

--- Assets/Script/Menu/Main_Menu.cs
+++ Assets/Script/Menu/Main_Menu.cs
@@ -6,10 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class Main_Menu : MonoBehaviour
 {
+
+    //３つのPanelを格納する変数
+    //インスペクターウィンドウからゲームオブジェクトを設定する
+    [SerializeField] GameObject M_Menu;
+    [SerializeField] GameObject Setting_Menu;
+    [SerializeField] GameObject Description_Menu;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
+        BackToMainMenu();
+       
     }
 
     // Update is called once per frame
@@ -18,7 +26,41 @@ public class Main_Menu : MonoBehaviour
         
     }
 
+    //ゲームシーンに遷移する
     public void GameStage(){
         SceneManager.LoadScene("O_FPS_1");
+    }
+
+    
+    
+    //ゲーム終了ボタンを押したら実行する
+    public void EndGame() {
+	    
+		#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_STANDALONE
+            UnityEngine.Application.Quit();
+         #endif
+	    
+    }
+
+    //ゲーム設定のメニュー
+    public void GameSetting(){
+        M_Menu.SetActive(false);
+        Setting_Menu.SetActive(true);
+        
+    }
+
+    //ゲーム説明のメニュー
+    public void GameDescription(){
+        M_Menu.SetActive(false);
+        Description_Menu.SetActive(true);
+    }
+
+    //メインメニューに戻る
+    public void BackToMainMenu(){
+        M_Menu.SetActive(true);
+        Setting_Menu.SetActive(false);
+        Description_Menu.SetActive(false);
     }
 }
