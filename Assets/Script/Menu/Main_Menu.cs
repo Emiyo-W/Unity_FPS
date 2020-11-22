9501d41d384c9923430e34d82a01a3eb3d649912
Emiyo-W <68138542+Emiyo-W@users.noreply.github.com>
2020-11-22 15:11:04 +0900

    Menuスクリプト
    MenuスクリプトとgamestageにLoadする機能を追加

---
+++ Assets/Script/Menu/Main_Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameStage(){
+        SceneManager.LoadScene("O_FPS_1");
+    }
+}
